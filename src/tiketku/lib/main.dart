2288b6690424f2a3e0e435a1825fe7227f064986
mohsamsulhadi <m.smslhd140903@gmail.com>
2023-10-30 11:28:02 +0700

    about dan splachscreen-novita

--- src/tiketku/lib/main.dart
+++ src/tiketku/lib/main.dart
@@ -1,8 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:tiketku/pages/home.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
   runApp(MaterialApp(initialRoute: '/', routes: {
-    '/': (context) => const HomePage(),
+    '/': (context) => const MyApp(),
   }));
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tentang Tiketku',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AboutPage(),
+    );
+  }
+}
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tentang Tiketku'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Kebijakan Privasi'),
+            onTap: () {
+              // Navigasi ke halaman Kebijakan Privasi
+            },
+          ),
+          ListTile(
+            title: Text('Syarat dan Ketentuan'),
+            onTap: () {
+              // Navigasi ke halaman Syarat dan Ketentuan
+            },
+          ),
+          ListTile(
+            title: Text('Kontak Kami'),
+            onTap: () {
+              // Navigasi ke halaman Kontak Kami
+            },
+          ),
+          ListTile(
+            title: Text('Versi Aplikasi'),
+            onTap: () {
+              // Tampilkan versi aplikasi
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text('Versi Aplikasi'),
+                    content: Text('1.0.0'), // Ganti dengan versi aktual aplikasi
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('Tutup'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
