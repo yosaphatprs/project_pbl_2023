c8a4dc781ddabdf0f5ae94cd0adfd456e1a6743a
NasyawaRamadhiaKiranaSavaEffendi <ramadhianasyawa@gmail.com>
2023-10-31 13:25:28 +0700

    Dashboard akun - nasyawa

--- src/tiketku/lib/main.dart
+++ src/tiketku/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tiketku/pages/home.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
@@ -7,17 +8,92 @@ void main() {
     '/': (context) => const MyApp(),
   }));
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tentang Tiketku',
+      title: 'Tiketku',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AboutPage(),
+      home: DashboardAkun(),
+    );
+  }
+}
+
+class DashboardAkun extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard Akun'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/profil.jpg'),
+                  radius: 50,
+                ),
+                SizedBox(height: 16.0),
+                Text(
+                  'Lek John',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                _buildListItem('Pusat Bantuan', () {}),
+                _buildListItem('Tentang', () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AboutPage(),
+                    ),
+                  );
+                }),
+                _buildListItem('Detail Akun', () {}),
+                InkWell(
+                  onTap: (){
+
+                  },
+                  child: Container(
+    padding: EdgeInsets.all(16.0),
+    color: Color(0xFFFF7A00), // Warna latar belakang untuk Logout
+    child: Text(
+      'Logout',
+      style: TextStyle(
+        color: Colors.white, // Warna teks untuk Logout
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+      ),
+       ),
+       ),
+       )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildListItem(String title, void Function() onTap) {
+    return ListTile(
+      title: Text(title),
+      onTap: onTap,
     );
   }
 }
@@ -58,7 +134,8 @@ class AboutPage extends StatelessWidget {
                 builder: (context) {
                   return AlertDialog(
                     title: Text('Versi Aplikasi'),
-                    content: Text('1.0.0'), // Ganti dengan versi aktual aplikasi
+                    content:
+                        Text('1.0.0'), // Ganti dengan versi aktual aplikasi
                     actions: [
                       TextButton(
                         onPressed: () {
